c9c8825726d73d2a6151fdc988c8ab29fc077a84
UdithDev <daveudith@gmail.com>
2024-07-24 01:30:27 +0530

    create footer section

--- Denta_clinic/App.tsx
+++ Denta_clinic/App.tsx
@@ -1,46 +1,17 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
 import LoginPage from './screens/LoginPage';
 import RegisterPage from './screens/RegisterPage';
-
-import {NavigationContainer} from '@react-navigation/native';
-import LandingPage from './screens/LandingPage';
 import AddUser from './screens/AddUser';
 import Inventory from './screens/Inventory';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import 'react-native-gesture-handler';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-
-const StackNav = () => {
-  const Stack = createNativeStackNavigator();
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        statusBarColor: '#1e608f',
-        headerStyle: {
-          backgroundColor: '#1e608f',
-        },
-        headerTintColor: '#fff',
-        headerTitleAlign: 'center',
-      }}>
-        <Stack.Screen name='Home' component={LandingPage}/>
-        <Stack.Screen name='Inventory' component={Inventory}/>
-      </Stack.Navigator>
-  );
-};
+import React from 'react';
+import LandingPage from './screens/LandingPage';
+import Footer from './components/Footer';
 
 function App() {
-  const Drawer=createDrawerNavigator();
   return (
     // <RegisterPage />
-    // <LoginPage/>
-
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Home' component={LandingPage}/>
-      </Drawer.Navigator>
-    </NavigationContainer>
+    // <LoginPage />
+    // <LandingPage/>
+    <Footer/>
   );
 }
 
--- Denta_clinic/screens/LandingPage.tsx
+++ Denta_clinic/screens/LandingPage.tsx
@@ -1,12 +1,27 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Footer from '../components/Footer';
 
 export default function LandingPage() {
   return (
-    <View>
-      <Text style={{color: 'black'}}>LandingPage</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>
+          fdfd
+        </Text>
+        <Text>
+          CSKLHVHDVH
+        </Text>
+      </View>
+      <Footer/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container:{
+    // flex:1,
+    backgroundColor:'white'
+  }
+});
