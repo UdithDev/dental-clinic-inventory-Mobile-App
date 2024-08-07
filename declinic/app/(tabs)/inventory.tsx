fa0166d0791ce9040e83bd9e91e8748f17824796
UdithDev <daveudith@gmail.com>
2024-08-07 07:37:43 +0530

    add scrollView Styles

--- declinic/app/(tabs)/inventory.tsx
+++ declinic/app/(tabs)/inventory.tsx
@@ -1,29 +1,35 @@
 import AddItem from "@/components/inventory/AddItem";
 import ItemForm from "@/components/inventory/ItemForm";
 import Welcome from "@/components/inventory/Welcome";
-import { useState,useRef } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Tab() {
   const [toggle, setToggle] = useState(true);
-  const printRef=useRef<View>(null)
+  const printRef = useRef<View>(null);
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-      <Welcome />
-      <AddItem />
-      <TouchableOpacity
-        onPress={() => setToggle(!toggle)}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Add Item</Text>
-      </TouchableOpacity>
-      {toggle && (
-        <View ref={printRef} style={styles.hiddenContent}>
-        <ItemForm/>
-        </View>
-      )}
+      <ScrollView style={styles.scrollView}>
+        <Welcome />
+        <AddItem />
+        <TouchableOpacity
+          onPress={() => setToggle(!toggle)}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Add Item</Text>
+        </TouchableOpacity>
+        {toggle && (
+          <View ref={printRef} style={styles.hiddenContent}>
+            <ItemForm />
+          </View>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
@@ -34,9 +40,9 @@ const styles = StyleSheet.create({
     // flex: 1,
   },
   buttonText: {
-    color: 'white', // text-white equivalent
+    color: "white", // text-white equivalent
     fontSize: 14, // text-sm equivalent
-    fontWeight: '500', // font-medium equivalent
+    fontWeight: "500", // font-medium equivalent
   },
   button: {
     backgroundColor: "#2D9596", // bg-accent equivalent color
@@ -44,9 +50,13 @@ const styles = StyleSheet.create({
     width: 96, // w-24 equivalent
     height: 32, // h-8 equivalent
     borderRadius: 9999, // rounded-full equivalent
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 16, // my-4 equivalent
+  },
+  scrollView: {
+    backgroundColor: "white",
+    marginHorizontal: 10,
   },
 
   hiddenContent: {
